6e156c0846fd2531306e3a0530c14bd41d4100db
Ricardo Belinato <ricbelinato@gmail.com>
2025-09-05 23:00:58 -0300

    chore: inicializa projeto React

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-600">
+      <h1 className="text-4xl font-bold text-white">Hello Tailwind 👋</h1>
+    </div>
+  )
+}
+
+export default App
