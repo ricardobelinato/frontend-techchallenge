86d44c291fb5c31b43e3114cc666540b29544db2
Ricardo Belinato <ricbelinato@gmail.com>
2025-09-17 20:42:15 -0300

    feat(ui): install Material UI and create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import Navbar from './components/Navbar.tsx'
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-blue-600">
-      <h1 className="text-4xl font-bold text-white">Hello Tailwind 👋</h1>
-    </div>
+    <Navbar/>
   )
 }
 
